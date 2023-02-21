2c05b949d2e93e6550e2ad3be9ef67428815d89f
zbari023 <barry450643@gmail.com>
2023-02-21 02:57:34 +0100

    new task

---
+++ py3.py
@@ -0,0 +1,10 @@
+print("My program, which give you wich number is greater")
+x = float(input("Enter your first number: x ="))
+y = float(input("Enter your second number:y ="))
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal y")
+else:
+    print("y is greater than x")
+
