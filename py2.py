114cd91f216ecb48e60c0251c815d5d296b814f2
zbari023 <barry450643@gmail.com>
2023-02-21 02:27:09 +0100

    a new work

---
+++ py2.py
@@ -0,0 +1,13 @@
+# input funktion: die eingegbene Werte in Run wird in den variabel gespeichert
+# Beispiel 1:
+# print("Anfang des Programms")
+# name = input("Bitte Ihre Name Eingeben:") ; age = input("Bitte Ihren Alter:")
+# print("Mein NAme ist " + name , "und ich bin " + age)
+# print("Ende des Programms")
+# ******************* #
+# Beispiel 2: Addierer, in dem zwei Zahlen addiert werden
+print("Welcome im my first Programm")
+value1 = float(input("Enter  your first Number:"))
+value2 = float(input("Enter your second Number:"))
+print("your get the value:", value1+value2)
+print("End Process")
