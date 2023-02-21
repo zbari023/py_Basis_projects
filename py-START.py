be50fbfa7ae8da9f78bc41796b7ef4eec4193b9c
zbari023 <barry450643@gmail.com>
2023-02-21 03:08:17 +0100

    Start

---
+++ py-START.py
@@ -0,0 +1,16 @@
+print("Hallo Welt")
+print("Hallo Python")
+print("Hallo" + " " + "Python" )            # Verkettung der Stringen
+print(2+2)
+print("Ziad")
+print(type(2))                              # ist ein integer und kann negativ ind positiv
+print(9/2)
+print(type(3/2))                            # ist ein float
+print((2+3)*9/5)
+print(12.2 * 5.77)
+print(type("Ziad"))                         # ist ein String
+print(type([1, 1, 2]))                      # ist ein liste
+x = 7 ; y = 5 ; z = 9; n = 8
+print(x+y+z+n)
+Name = "Ziad" ; age = 27 ; ID = 53424
+print("Mein name ist" + " " + Name , ID , "und ich bin",age )   # int + str, indem " , " dazwischen
