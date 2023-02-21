318fbd1a573a050efa50834f21e3ee8edb24d9b4
zbari023 <barry450643@gmail.com>
2023-02-21 03:54:25 +0100

    simple programm

---
+++ py4.py
@@ -0,0 +1,13 @@
+# while schleife
+
+# counter = 5                            # Anzahl der angegebenen print (ausgeführten Code)
+# while counter < 10:                    # sobald diese Bedingung true ist dann wird weiter ausgeführt
+#    print("Hier der Code, der wiederholst ausgeführt werden soll")
+#    counter = counter + 1              # Zähler
+
+x = int(input("How many times do you want to repeat your Name: "))
+y = 0
+name = input("Enter your Name: ")
+while  y < x:
+    print(name)
+    y = y + 1
