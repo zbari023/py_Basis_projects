25993f9afce997b4ff3458e9f7748ca27694695f
zbari023 <barry450643@gmail.com>
2023-02-21 23:06:16 +0100

    Listen anf for loop

---
+++ py5.py
@@ -0,0 +1,10 @@
+# Listen und ausgeführten Befehle:
+nameList = ["ziad" , "Andree" , "stefan" , 7 , 8.3]  # kann veschiedene variabeln enthalten
+print(nameList)
+print(nameList[-1])  # Egal wie viel Elemente wir haben mit -1 greifen wir den letzten Element
+print(nameList[0])  # wird von 0 angefangen um den ersten stelle zu greifen
+
+
+# for Schleife
+for x in range(0,10,1):   # im Kopf steht ein range-funktion, die Start, Ende und den zählerschritt enthält
+    print(x)
