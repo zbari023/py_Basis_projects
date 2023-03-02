8d4ad00e5a665c1cf1018aa8faa2dae8eb971559
zbari023 <barry450643@gmail.com>
2023-03-02 21:51:24 +0100

    new work

---
+++ py9_training.py
@@ -0,0 +1,25 @@
+# to make the letter capitel
+name = "ziad"
+print(name.upper())
+# to make the letter small
+name2 ="BARI"
+print(name2.lower())
+# to make the first letter in str capital
+name3 ="ziad"
+print(name3.capitalize())
+# to make the first letter in each member in str capital
+name4 = "ziad ibrahim bari"
+print(name4.title())
+# to covert the str to list
+name5 = "ziad ibrahim bari"
+print(name5.split())  # between the splite is the space maker
+# # ['ziad', 'ibrahim', 'bari']
+name6="html and css and JS and python"
+print(name6.split("and"))  # and here is the space maker
+# ['html ', ' css ', ' JS ', ' python']
+# to know how many lettel in str
+name7="html and css and JS and python" # space is also in your len
+print(len(name7))
+# to replace a word or somthing like that
+name8="html and css and JS and python"
+print(name8.replace("python", "php"))  # html and css and JS and php
