e9c7b510964b0736f96745c9dd954d446418855a
zbari023 <barry450643@gmail.com>
2023-03-02 22:13:40 +0100

    new task for number in python

---
+++ py_10_number.py
@@ -0,0 +1,18 @@
+# Verkettung num and str
+from math import *
+user="ziad"
+age=27
+print("i am" + " " + user + " and i am " + str(age) )  # convert the int to str for "Verkettung"
+
+# round function
+print(round(4.6))  # 5
+# floor ,ceil function
+print(floor(3.9))  # 3
+print(ceil(3.9))   # 4
+# pow "Exponent"
+print(pow(2,5))
+# Betraf
+print(abs(-3))
+# min and max
+print(max(4,5))
+print(min(88,100))
