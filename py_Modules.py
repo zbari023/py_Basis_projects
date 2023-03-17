47344ef9c3773f0084390fa106d75682ab6d3c64
zbari023 <barry450643@gmail.com>
2023-03-17 22:22:45 +0100

    new work

--- py_Modules.py
+++ py_Modules.py
@@ -1,2 +1,7 @@
 import os
-
+os.mkdir('New_folder')
+writing = open('test10.txt','w')
+writing = file.write(' \n i am here and \n My name is ziad Ibrahim Bari from syria, 27, student, married')
+print(writing)
+print(writing.tell())
+writing.close()
