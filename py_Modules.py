ac3cc55a2ee9a17a4339f08762999146f5c9a3ab
zbari023 <barry450643@gmail.com>
2023-03-21 15:30:00 +0100

    read from file

--- py_Modules.py
+++ py_Modules.py
@@ -1,7 +1,4 @@
 import os
-os.mkdir('New_folder')
-writing = open('test10.txt','w')
-writing = file.write(' \n i am here and \n My name is ziad Ibrahim Bari from syria, 27, student, married')
-print(writing)
-print(writing.tell())
-writing.close()
+file = open('test2.txt','r')
+words = file.read(30)
+print(words)
