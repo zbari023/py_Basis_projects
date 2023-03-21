0bf64b78a378c453acdabe9af27ceb9c904ffc7c
zbari023 <barry450643@gmail.com>
2023-03-21 15:47:09 +0100

    new work

--- py_Modules.py
+++ py_Modules.py
@@ -1,4 +1,14 @@
 import os
-file = open('test2.txt','r')
-words = file.read(30)
+# to read from file
+file = open('test2.txt','r')    # read from file using 'r' or 'r+'
+print(file.tell())
+words = file.readlines()        # just a special words , a part from file , or readlines for all of file
+print(file.tell())              # to know where is the position of mouse
 print(words)
+
+
+
+# to write in file
+file2 = open('test1.txt','w')
+words2 = file2.write('I am here today\nI am ziad bari')
+file2.close()
