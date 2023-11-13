11c10711504358d6526bc7689cdbc5788cef867a
Ziad Bari <barry450643@gmail.com>
2023-11-13 23:46:55 +0100

    Update py_documentation2.py

--- py_documentation2.py
+++ py_documentation2.py
@@ -1,1 +1,17 @@
+# documentation / files
+import os
+
+file = open('test.txt','r')
+print(file.tell())
+word = file.readlines()
+print(file.tell())
+print(word)
+file.close()
+# file = open('test.txt','w')
+file = open('test.txt','a')
+writing = file.write(' \n i am here and \n My name is ziad Ibrahim Bari from syria, 27, student, married')
+print(writing)
+print(file.tell())
+
+file.close()
 
