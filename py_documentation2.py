89c1f8b38539774ecda15450408b87525865f27c
Ziad Bari <barry450643@gmail.com>
2023-11-13 23:46:30 +0100

    Update py_documentation2.py

--- py_documentation2.py
+++ py_documentation2.py
@@ -12,4 +12,4 @@ file = open('test.txt','a')
 writing = file.write(' \n i am here and \n My name is ziad Ibrahim Bari from syria, 27, student, married')
 print(writing)
 print(file.tell())
-file.close()
+
